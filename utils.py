ca4247196cc990a5f8380ec5c1a9e4909380939f
Kalekdan <joerickard@hotmail.co.uk>
2021-11-08 18:18:14 +0000

    Added getting ratings to a dictionary

--- utils.py
+++ utils.py
@@ -30,3 +30,25 @@ def getWatchedFilms(username):
 def getRatedFilms(username):
     return getPageFilms(username, "films/ratings", "poster-list -p150 -grid")
 
+def getRatings(username):
+    ratings = {}
+    pageNum = 1
+    urlPath = "films/ratings"
+    divClass = "poster-list -p150 -grid"
+    while True:
+        print("Scraping " + username + " " + urlPath + " page: " + str(pageNum))
+        watchlistURL = letterboxdURL + username + '/'+ urlPath +'/page/' + str(pageNum)
+        page = requests.get(watchlistURL)
+        soup = BeautifulSoup(page.content, "html.parser")
+        listHTML = soup.find_all("ul", {"class": divClass})
+        if len(listHTML) == 0:
+            return ratings
+        li = listHTML[0].findChildren("li")
+        if len(li) == 0:
+            return ratings
+        for film in li:
+            name = film.find('img')['alt']
+            rating = int(film.find('span', {"class":"rating"})['class'][1].split('-')[1])
+            ratings[name] = rating
+        pageNum = pageNum + 1
+
