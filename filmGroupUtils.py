aecc80f00a0c9c18f827ffa9a5f1588c1ebb8ef7
Kalekdan <joerickard@hotmail.co.uk>
2022-01-27 13:11:03 +0000

    Initial UI Built

--- filmGroupUtils.py
+++ filmGroupUtils.py
@@ -3,15 +3,24 @@ class filmGroupMember:
         self.name = name
         self.letterboxdUsername = letterboxdUsername
 
+
 class filmGroup:
-    def __init__(self, groupName, members):
+    def __init__(self, groupName, members=None):
         self.groupName = groupName
         self.members = members
+
     def addMember(self, newMember):
         if newMember in self.members:
             return
         self.members.apend(newMember)
+
     def removeMember(self, memberToRemove):
         if memberToRemove in self.members:
             self.members.remove(memberToRemove)
         return
+
+    def getMembers(self):
+        return self.members
+
+    def __str__(self):
+        return self.groupName
--- mainWindow.py
+++ mainWindow.py
@@ -1,8 +1,28 @@
+from tkinter import *
 import tkinter
+from tkinter import ttk
 
-m = tkinter.Tk()
+import filmGroupUtils
 
 
+def addNewGroupToList(groupList, newGroupName):
+    gl = groupList.append(filmGroupUtils.filmGroup(newGroupName))
+    filmGroupList = gl
+
+mm = filmGroupUtils.filmGroup("Movie Mondays")
+group2 =filmGroupUtils.filmGroup("Family Movie Group")
+filmGroupList = []
+
+root = tkinter.Tk()
+frm = ttk.Frame(root, padding=10)
+frm.grid()
+groupNameEntry = ttk.Entry(frm)
+groupNameEntry.grid(column=0, row=0)
+groupComboBox = ttk.Combobox(frm, values=filmGroupList).grid(column=0, row=1)
+ttk.Button(frm, text="Create new group", command=lambda: addNewGroupToList(filmGroupList, groupNameEntry.get())).grid(column=1, row=0)
+
+ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=1)
+# ttk.Button(frm, text="aw", command=lambda: printtext("4")).grid(column=2, row=0)
 
 
-m.mainloop()
+root.mainloop()
